12190e5d7b05610fae60e9d51b1e09e143f25dbe
Barta Balázs <barta.balazs00@gmail.com>
2021-10-05 14:40:47 +0200

    felvesz function létrehozása

--- 2021.10.05 dolgozat/main.js
+++ 2021.10.05 dolgozat/main.js
@@ -14,7 +14,41 @@ filmek.push(obj1);
 
 console.log(filmek[0].nev);
 
+function felvesz(){
+    let hiba = false;
+    let nev = document.getElementById("nev").value;
+    let rendezo = document.getElementById("rendezo").value;
+    let kiadasiev = document.getElementById("kiadasiEv").value;
+    if(nev == ""){
+        document.getElementById("nevError").innerHTML="A név nem lehet üres";
+        hiba = true;
+    } else {
+        document.getElementById("nevError").innerHTML="";
+        hiba = false;
+    }
 
+    if(rendezo == ""){
+        document.getElementById("rendezoError").innerHTML="A rendező nem lehet üres";
+        hiba = true;
+    } else {
+        document.getElementById("rendezoError").innerHTML="";
+        hiba = false;
+    }
+
+    if(kiadasiev == ""){
+        document.getElementById("kiadasiEvError").innerHTML="A kiadási év nem lehet üres";
+        hiba = true;
+    } else {
+        document.getElementById("kiadasiEvError").innerHTML="";
+        hiba = false;
+    }
+
+    if(!hiba){
+        let obj = new Film(nev,rendezo,kiadasiev);
+        filmek.push(obj);
+    }
+    
+}
 
 
 document.getElementById("kuld").addEventListener("click", felvesz);
