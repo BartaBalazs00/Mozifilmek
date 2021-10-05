6c0a1085af9b1d5fe09847c84de2b1d9d57f0d44
Barta Balázs <barta.balazs00@gmail.com>
2021-10-05 17:40:02 +0200

    dizájn hozzáadása

--- 2021.10.05 dolgozat/main.js
+++ 2021.10.05 dolgozat/main.js
@@ -16,44 +16,46 @@ console.log(filmek[0].nev);
 
 function felvesz(){
     let date = new Date();
-    let hiba = false;
     let nev = document.getElementById("nev").value;
+    let nevhiba = false;
     let rendezo = document.getElementById("rendezo").value;
+    let rendeziHiba = false;
     let kiadasiev = document.getElementById("kiadasiEv").value;
+    let kiadasiEvHiba = false;
     if(nev == ""){
         document.getElementById("nevError").innerHTML="A név nem lehet üres";
-        hiba = true;
+        nevhiba = true;
     } else {
         document.getElementById("nevError").innerHTML="";
-        hiba = false;
+        nevhiba = false;
     }
 
     if(rendezo == ""){
         document.getElementById("rendezoError").innerHTML="A rendező nem lehet üres";
-        hiba = true;
+        rendeziHiba = true;
     } else {
         document.getElementById("rendezoError").innerHTML="";
-        hiba = false;
+        rendeziHiba = false;
     }
 
     if(kiadasiev == ""){
         document.getElementById("kiadasiEvError").innerHTML="A kiadási év nem lehet üres";
-        hiba = true;
+        kiadasiEvHiba = true;
     } else if(isNaN(kiadasiev)){
         document.getElementById("kiadasiEvError").innerHTML="A kiadási év szám kell hogy legyen";
-        hiba = true;
-    } else if(kiadasiev>date.getFullYear()+1){
+        kiadasiEvHiba = true;
+    } else if(kiadasiev>date.getFullYear()){
         document.getElementById("kiadasiEvError").innerHTML="Nem lehet a kiadási év nagyobb mint az aktuális dátum";
-        hiba = true;
+        kiadasiEvHiba = true;
     } else if(kiadasiev<1888){
         document.getElementById("kiadasiEvError").innerHTML="Ilyenkor még nem is csináltak filmeket XD";
-        hiba = true;
+        kiadasiEvHiba = true;
     } else {
         document.getElementById("kiadasiEvError").innerHTML="";
-        hiba = false;
+        kiadasiEvHiba = false;
     }
 
-    if(!hiba){
+    if(!nevhiba && !rendeziHiba && !kiadasiEvHiba){
         let obj = new Film(nev,rendezo,kiadasiev);
         filmek.push(obj);
         document.getElementById("nev").value="";
@@ -61,13 +63,15 @@ function felvesz(){
         document.getElementById("kiadasiEv").value="";
         
     }
-    let tablazat = "<table><tr><th>Név</th><th>Rendező</th><th>Kiadási év</th></tr>";
+    let tablazat = "<table class='table table-striped table-hover align-middle'>";
+    tablazat+="<thead class='table-dark align-middle'><tr><th>Név</th><th>Rendező</th>";
+    tablazat+="<th>Kiadási év</th></tr></thead><tbody>";
         for (let i = 0; i < filmek.length; i++) {
             tablazat+="<tr> <td>"+filmek[i].nev+"</td>";
             tablazat+="<td>"+filmek[i].rendezo+"</td>";
             tablazat+="<td>"+filmek[i].kiadasiev+"</td></tr>";
         }
-        tablazat+="</table>";
+        tablazat+="</tbody></table>";
         document.getElementById("tablazat").innerHTML = tablazat;
 }
 
