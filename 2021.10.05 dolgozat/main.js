ec00e7d8eb5ae15ccf69448c456ce8cc71a355aa
Barta Balázs <barta.balazs00@gmail.com>
2021-10-05 16:06:03 +0200

    validálás, listázz gomb törlése

--- 2021.10.05 dolgozat/main.js
+++ 2021.10.05 dolgozat/main.js
@@ -15,6 +15,7 @@ filmek.push(obj1);
 console.log(filmek[0].nev);
 
 function felvesz(){
+    let date = new Date();
     let hiba = false;
     let nev = document.getElementById("nev").value;
     let rendezo = document.getElementById("rendezo").value;
@@ -38,6 +39,15 @@ function felvesz(){
     if(kiadasiev == ""){
         document.getElementById("kiadasiEvError").innerHTML="A kiadási év nem lehet üres";
         hiba = true;
+    } else if(isNaN(kiadasiev)){
+        document.getElementById("kiadasiEvError").innerHTML="A kiadási év szám kell hogy legyen";
+        hiba = true;
+    } else if(kiadasiev>date.getFullYear()+1){
+        document.getElementById("kiadasiEvError").innerHTML="Nem lehet a kiadási év nagyobb mint az aktuális dátum";
+        hiba = true;
+    } else if(kiadasiev<1888){
+        document.getElementById("kiadasiEvError").innerHTML="Ilyenkor még nem is csináltak filmeket XD";
+        hiba = true;
     } else {
         document.getElementById("kiadasiEvError").innerHTML="";
         hiba = false;
@@ -49,10 +59,16 @@ function felvesz(){
         document.getElementById("nev").value="";
         document.getElementById("rendezo").value="";
         document.getElementById("kiadasiEv").value="";
+        
     }
-    
+    let tablazat = "<table><tr><th>Név</th><th>Rendező</th><th>Kiadási év</th></tr>";
+        for (let i = 0; i < filmek.length; i++) {
+            tablazat+="<tr> <td>"+filmek[i].nev+"</td>";
+            tablazat+="<td>"+filmek[i].rendezo+"</td>";
+            tablazat+="<td>"+filmek[i].kiadasiev+"</td></tr>";
+        }
+        tablazat+="</table>";
+        document.getElementById("tablazat").innerHTML = tablazat;
 }
-
-
 
 document.getElementById("kuld").addEventListener("click", felvesz);
